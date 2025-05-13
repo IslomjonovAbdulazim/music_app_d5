7d112b30fa35fd0200c9448166cee0bb56e3c0f0
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-13 17:18:49 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/music_app_d5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.music_app_d5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:music_app_d5/pages/auth/start_page.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StartPage(),
+    );
+  }
+}
