092f5d245ffc56881eeca3c780b3ee4f524d868f
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-13 17:44:38 +0500

    imp

--- lib/pages/auth/start_page.dart
+++ lib/pages/auth/start_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({super.key});
@@ -14,17 +16,54 @@ class _StartPageState extends State<StartPage> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/spotify.png",
-              ),
-            ],
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Image.asset(
+                  "assets/spotify.png",
+                  width: 240,
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "Enjoy listening to music",
+                  style: GoogleFonts.quicksand(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 26,
+                    color: Color(0xff383838),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Spotify is a proprietary Swedish audio streaming and media services provider",
+                  style: GoogleFonts.quicksand(
+                    color: Color(0xff797979),
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 70,
+                        child: CupertinoButton(
+                          color: Color(0xff42C83C),
+                          borderRadius: BorderRadius.circular(20),
+                          onPressed: () {},
+                          child: Text("Register"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
