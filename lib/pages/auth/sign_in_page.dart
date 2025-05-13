fbe4c42cc05fcde747aadf868a8abcf4070fbd46
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-13 18:27:58 +0500

    imp

--- lib/pages/auth/sign_in_page.dart
+++ lib/pages/auth/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
@@ -11,6 +12,28 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.all(5),
+          child: CupertinoButton(
+            padding: EdgeInsets.zero,
+            color: Colors.grey.shade100,
+            borderRadius: BorderRadius.circular(100),
+            onPressed: () {},
+            child: Icon(
+              CupertinoIcons.chevron_left,
+              color: Color(0xff414141),
+            ),
+          ),
+        ),
+        centerTitle: true,
+        title: Image.asset(
+          "assets/spotify.png",
+          height: 33,
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
