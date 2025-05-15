df03f7b1e3059128fdff1c4657f5a1aac346482e
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-15 19:02:28 +0500

    imp

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -31,6 +33,44 @@ class _ProfilePageState extends State<ProfilePage> {
                     bottomLeft: Radius.circular(50),
                   ),
                 ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      // Avatar
+
+                      // Email
+
+                      // Fullname
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "778",
+                                style: GoogleFonts.quicksand(
+                                  color: Color(0xff222222),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                "Followers",
+                                style: GoogleFonts.quicksand(
+                                  color: Color(0xff585858),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
@@ -40,18 +80,46 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 }
 
+class _MusicItem extends StatelessWidget {
+  final String image;
+  final String title;
+  final String subtitle;
+  final String duration;
 
+  const _MusicItem(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.subtitle,
+      required this.duration});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      onPressed: () {},
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              image,
+              height: 56,
+              width: 56,
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title),
+                Text(subtitle),
+              ],
+            ),
+          ),
+          Text(duration),
+          Icon(Icons.linear_scale_sharp),
+        ],
+      ),
+    );
+  }
+}
