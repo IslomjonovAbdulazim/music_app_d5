9f1a711c8664c1adbd78d6ca14fe68c7797a9cb0
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-17 19:08:55 +0500

    imp

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,78 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: CupertinoButton(
+          onPressed: () {},
+          child: Icon(
+            CupertinoIcons.search,
+            color: Colors.black,
+            size: 30,
+          ),
+        ),
+        title: Image.asset(
+          "assets/spotify.png",
+          height: 40,
+        ),
+        actions: [
+          CupertinoButton(
+            onPressed: () {},
+            child: Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 25,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 190,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    height: 120,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Color(0xff42C83C),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("New Album"),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 170,
+                              child: Text("Happier Than Ever"),
+                            ),
+                          ],
+                        ),
+                        Text("Billie Eilish"),
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Image.asset(
+                      "assets/home-3.png",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
