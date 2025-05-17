63fe9d953dd5367a63c9a4ffc1c8965018c6f8d7
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-17 18:32:13 +0500

    imp

--- lib/pages/home/liked_page.dart
+++ lib/pages/home/liked_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -70,27 +71,120 @@ class _LikedPageState extends State<LikedPage> {
               ),
             ],
           ),
-
+          SizedBox(height: 10),
+          SizedBox(
+            height: 170,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              children: [
+                _Album(
+                  image: "assets/liked-1.png",
+                  title: "Happier Than Ever",
+                ),
+                SizedBox(width: 20),
+                _Album(
+                  image: "assets/liked-1.png",
+                  title: "Happier Than Ever",
+                ),
+                SizedBox(width: 20),
+                _Album(
+                  image: "assets/liked-1.png",
+                  title: "Happier Than Ever",
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
         ],
       ),
     );
   }
 }
 
+class _Album extends StatelessWidget {
+  final String image;
+  final String title;
 
+  const _Album({
+    super.key,
+    required this.image,
+    required this.title,
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Image.asset(
+            image,
+            height: 130,
+            width: 130,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(height: 6),
+        Text(
+          title,
+          style: GoogleFonts.quicksand(
+            color: Colors.black,
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
 
+class _Song extends StatelessWidget {
+  final String title;
+  final String artist;
+  final String duration;
+  final bool isLiked;
+  final VoidCallback onTap;
 
+  const _Song({
+    super.key,
+    required this.title,
+    required this.artist,
+    required this.duration,
+    required this.isLiked,
+    required this.onTap,
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      onPressed: () {},
+      child: Row(
+        children: [
+          // Play - Container
+          Expanded(
+            child: Column(
+              children: [
+                // Title - Text
+                // Artist - Text
+              ],
+            ),
+          ),
+          // Duration - Text
 
-
-
-
-
-
-
-
-
-
-
-
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              onTap();
+            },
+            child: Icon(
+              isLiked ? CupertinoIcons.heart_fill : CupertinoIcons.heart,
+              color: isLiked ? Colors.red : Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
